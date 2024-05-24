2deb6a71ad478299d1053c25fe74e01b3bf51d3c
Patrick Morris <telavian@gmail.com>
2024-05-24 07:57:57 -0700

    Moved common display items to ActionDisplay

--- src/Liquidata.Client/Pages/ActionDisplay/ActionDisplay.razor.cs
+++ src/Liquidata.Client/Pages/ActionDisplay/ActionDisplay.razor.cs
@@ -1,6 +1,8 @@
-﻿using Liquidata.Client.Pages.Common;
+﻿using Liquidata.Client.Extensions;
+using Liquidata.Client.Pages.Common;
 using Liquidata.Common.Actions;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Liquidata.Client.Pages.ActionDisplay;
 
@@ -12,11 +14,44 @@ public partial class ActionDisplayViewModel : ViewModelBase
     [Parameter]
     public EditProjectViewModel? Parent { get; set; }
 
+    public bool IsMouseOver { get; set; }
+
+    private Func<bool, Task>? _updateIsMouseOverAsyncCommand;
+    public Func<bool, Task> UpdateIsMouseOverAsyncCommand => _updateIsMouseOverAsyncCommand ??= CreateEventCallbackAsyncCommand<bool>(UpdateIsMouseOverAsync, "Unable to update is mouse over");
+
+
     protected override async Task OnParametersSetAsync()
     {
         await base.OnParametersSetAsync();
 
         // TODO: Optimize
         await RefreshAsync();
+    }
+
+    protected string BuildItemVisibility(params bool[] states)
+    {
+        var anyTrue = states.Any(x => x);
+
+        var style = @"margin-right: -15px; ";
+
+        return anyTrue
+            ? $"{style} display: block"
+            : $"{style} display: none";
+    }
+
+    protected string BuildActionIcon()
+    {
+        return Action!.ActionType.BuildActionIcon();
+    }
+
+    protected Color BuildActionColor()
+    {
+        return Action!.ActionType.BuildActionColor();
+    }
+
+    private async Task UpdateIsMouseOverAsync(bool isOver)
+    {
+        await Task.Yield();
+        IsMouseOver = isOver;
     }
 }
--- src/Liquidata.Client/Pages/ActionDisplay/ActionDisplayViewModelBase.cs
+++ src/Liquidata.Client/Pages/ActionDisplay/ActionDisplayViewModelBase.cs
@@ -16,11 +16,7 @@ namespace Liquidata.Client.Pages.ActionDisplay
         public EditProjectViewModel? Parent { get; set; }
 
         public T TypedAction => (T)Action!;
-        public bool IsMouseOver { get; set; }        
-
-        private Func<bool, Task>? _updateIsMouseOverAsyncCommand;
-        public Func<bool, Task> UpdateIsMouseOverAsyncCommand => _updateIsMouseOverAsyncCommand ??= CreateEventCallbackAsyncCommand<bool>(UpdateIsMouseOverAsync, "Unable to update is mouse over");
-
+        
         protected override async Task OnParametersSetAsync()
         {
             await base.OnParametersSetAsync();
@@ -28,31 +24,5 @@ namespace Liquidata.Client.Pages.ActionDisplay
             // TODO: Optimize
             await RefreshAsync();
         }
-
-        protected string BuildItemVisibility(params bool[] states)
-        {
-            var anyTrue = states.Any(x => x);
-
-            return anyTrue
-                ? "display: block"
-                : "display: none";
-        }
-
-        protected string BuildActionIcon()
-        {
-            return Action!.ActionType.BuildActionIcon();
-        }
-
-        protected Color BuildActionColor()
-        {
-            return Action!.ActionType.BuildActionColor();
-        }
-
-        private async Task UpdateIsMouseOverAsync(bool isOver)
-        {
-            await Task.Yield();
-            IsMouseOver = isOver;
-        }
-
     }
 }
