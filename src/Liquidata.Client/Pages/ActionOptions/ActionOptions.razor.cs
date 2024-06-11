a5149bac4c586bcafd12a4b463cd01b6889fbe5c
Patrick Morris <telavian@gmail.com>
2024-06-11 10:43:38 -0700

    Added action options dialog

--- src/Liquidata.Client/Pages/ActionOptions/ActionOptions.razor.cs
+++ src/Liquidata.Client/Pages/ActionOptions/ActionOptions.razor.cs
@@ -1,6 +1,8 @@
 ﻿using Liquidata.Client.Pages.Common;
+using Liquidata.Client.Pages.Dialogs;
 using Liquidata.Common.Actions.Shared;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Liquidata.Client.Pages.ActionOptions;
 
@@ -12,11 +14,27 @@ public partial class ActionOptionsViewModel : ViewModelBase
     [Parameter]
     public EditProjectViewModel? Parent { get; set; }
 
+    private Func<Task>? _openOptionsInDialogAsyncCommand;
+    public Func<Task> OpenOptionsInDialogAsyncCommand => _openOptionsInDialogAsyncCommand ??= CreateEventCallbackAsyncCommand(HandleOpenOptionsInDialogAsync, "Unable to open options");
+
     protected override async Task OnParametersSetAsync()
     {        
         await base.OnParametersSetAsync();
 
         // TODO: Optimize
         await RefreshAsync();
+    }
+
+    private async Task HandleOpenOptionsInDialogAsync()
+    {
+        await Task.Yield();
+
+        var parameters = new DialogParameters
+        {
+            { nameof(ActionOptionsDialogViewModel.Action), Action },
+            { nameof(ActionOptionsDialogViewModel.Parent), Parent }
+        };
+
+        await ShowDialogAsync<ActionOptionsDialog>("Options", parameters);
     }
 }
---
+++ src/Liquidata.Client/Pages/Dialogs/ActionOptionsDialog.razor.cs
@@ -0,0 +1,11 @@
+﻿
+using Liquidata.Common.Actions.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace Liquidata.Client.Pages.Dialogs;
+
+public class ActionOptionsDialogViewModel : DialogViewModelBase
+{
+    [Parameter] public ActionBase Action { get; set; } = null!;
+    [Parameter] public EditProjectViewModel Parent { get; set; } = null!;
+}
