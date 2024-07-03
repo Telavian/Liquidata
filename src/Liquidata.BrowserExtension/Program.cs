471291f5f85e90a5b63dffc52ab2e88fdf7804ac
Patrick Morris <telavian@gmail.com>
2024-07-02 17:05:24 -0700

    Added browser extension placeholder

---
+++ src/Liquidata.BrowserExtension/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Liquidata.BrowserExtension
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.UseBrowserExtension(browserExtension =>
+            {
+                builder.RootComponents.Add<App>("#app");
+                builder.RootComponents.Add<HeadOutlet>("head::after");
+            });
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ src/Liquidata.BrowserExtension/wwwroot/BackgroundWorker.js
@@ -0,0 +1,49 @@
+﻿// Import for the side effect of defining a global 'browser' variable
+import * as _ from "/content/Blazor.BrowserExtension/lib/browser-polyfill.min.js";
+
+// Import for the netrequest api
+import "/data/scripts/netrequest.js";
+
+browser.runtime.onInstalled.addListener(() => {
+    console.log("Extension installed");
+
+    const indexPageUrl = browser.runtime.getURL("index.html");
+    browser.tabs.create({
+        url: indexPageUrl
+    });
+});
+
+browser.runtime.onStartup.addListener(() => {
+    console.log("Extension started");
+
+    const domains = ['telavian.github.io', 'localhost'];
+
+    // Chromium
+    app.netrequest.rules.push({
+        "condition": {
+            "urlFilter": '*',
+            "initiatorDomains": domains, 
+        },
+        "action": {
+            "type": "modifyHeaders",
+            "responseHeaders": [{
+                "operation": "remove",
+                "header": "X-Frame-Options"
+            }]
+        }
+    });
+
+    app.netrequest.rules.push({
+        "condition": {
+            "urlFilter": '*',
+            "initiatorDomains": domains, 
+        },
+        "action": {
+            "type": "modifyHeaders",
+            "responseHeaders": [{
+                "operation": "remove",
+                "header": "Frame-Options"
+            }]
+        }
+    });
+});
