76c7e11f4a7d1877c0d507ed1427b0aa2e55fd11
Patrick Morris <telavian@gmail.com>
2024-07-02 18:08:58 -0700

    Switched to 1.5.0 for browser extension

---
+++ src/Liquidata.BrowserExtension/BackgroundWorker.cs
@@ -0,0 +1,91 @@
+﻿using Blazor.BrowserExtension;
+using System;
+using System.Threading.Tasks;
+using WebExtensions.Net.WebRequest;
+
+namespace Liquidata.BrowserExtension;
+
+public partial class BackgroundWorker : BackgroundWorkerBase
+{
+    private readonly string[] _domains = ["telavian.github.io", "localhost"];
+    private readonly string[] _headers = ["Frame-Options"];
+
+    [BackgroundWorkerMain]
+    public override void Main()
+    {
+        WebExtensions.Runtime.OnInstalled.AddListener(OnInstalled);
+        WebExtensions.Runtime.OnStartup.AddListener(OnStartup);
+    }
+
+    async Task OnInstalled()
+    {
+        var indexPageUrl = await WebExtensions.Runtime.GetURL("index.html");
+        await WebExtensions.Tabs.Create(new()
+        {
+            Url = indexPageUrl
+        });
+    }    
+
+    async Task OnStartup()
+    {
+        await WebExtensions.WebRequest.OnBeforeSendHeaders.AddListener(HandleOnBeforeSendHeaders);
+        await WebExtensions.WebRequest.OnHeadersReceived.AddListener(HandleOnHeadersReceived);
+    }    
+
+    private BlockingResponse HandleOnBeforeSendHeaders(OnBeforeSendHeadersEventCallbackDetails details)
+    {
+        if (!IsUrlMatch(details.Url) && !IsUrlMatch(details.DocumentUrl) && !IsUrlMatch(details.OriginUrl))
+        {
+            return new();
+        }
+
+        return new BlockingResponse
+        {
+            RequestHeaders = FilterHeaders(details.RequestHeaders),
+        };
+    }
+
+    private BlockingResponse HandleOnHeadersReceived(OnHeadersReceivedEventCallbackDetails details)
+    {
+        if (!IsUrlMatch(details.Url) && !IsUrlMatch(details.DocumentUrl) && !IsUrlMatch(details.OriginUrl))
+        {
+            return new();
+        }
+
+        return new BlockingResponse
+        {
+            ResponseHeaders = FilterHeaders(details.ResponseHeaders),
+        };
+    }
+
+    private bool IsUrlMatch(string url)
+    {
+        var uri = new Uri(url, UriKind.RelativeOrAbsolute);
+        if (!uri.IsAbsoluteUri)
+        {
+            return false;
+        }
+
+        var host = uri.Host;
+        
+        foreach (var domain in _domains)
+        {
+            if (host.EndsWith(domain, StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private HttpHeaders FilterHeaders(HttpHeaders headers)
+    {
+        foreach (var header in _headers)
+        {
+            headers.RemoveAll(x => x.Name.Contains(header, StringComparison.OrdinalIgnoreCase));
+        }
+        
+        return headers;
+    }
+}
--- src/Liquidata.BrowserExtension/Program.cs
+++ src/Liquidata.BrowserExtension/Program.cs
@@ -1,26 +1,35 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿using Blazor.BrowserExtension;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.ComponentModel;
 using System.Net.Http;
 using System.Threading.Tasks;
 
-namespace Liquidata.BrowserExtension
-{
-    public static class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+namespace Liquidata.BrowserExtension;
 
-            builder.UseBrowserExtension(browserExtension =>
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        await Task.Yield();
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+        builder.UseBrowserExtension(browserExtension =>
+        {
+            if (browserExtension.Mode == BrowserExtensionMode.Background)
+            {
+                builder.RootComponents.AddBackgroundWorker<BackgroundWorker>();
+            }
+            else
             {
                 builder.RootComponents.Add<App>("#app");
                 builder.RootComponents.Add<HeadOutlet>("head::after");
-            });
+            }
+        });
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            await builder.Build().RunAsync();
-        }
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        await builder.Build().RunAsync();
     }
 }
