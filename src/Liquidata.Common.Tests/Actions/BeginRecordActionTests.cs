4f7f10ae5188bac31289e8aced2fd7fc4b767b05
Patrick Morris <telavian@gmail.com>
2024-07-05 11:59:34 -0700

    Added placeholder tests

---
+++ src/Liquidata.Common.Tests/Actions/BeginRecordActionTests.cs
@@ -0,0 +1,34 @@
+using Liquidata.Common.Actions;
+using Liquidata.Common.Actions.Enums;
+using Liquidata.Common.Services.Interfaces;
+using Moq;
+
+namespace Liquidata.Common.Tests.Actions
+{
+    public class BeginRecordActionTests
+    {
+        [Fact]
+        public async Task GivenCall_WhenValidation_ThenNoErrors()
+        {
+            await Task.Yield();
+            var action = new BeginRecordAction();
+            var errors = action.BuildValidationErrors();
+
+            Assert.Empty(errors);
+        }
+
+        [Fact]
+        public async Task GivenCall_WhenExecuted_ThenRecordAdded()
+        {
+            var dataHandler = new Mock<IDataHandlerService>();
+            var executionService = new Mock<IExecutionService>();            
+            executionService.Setup(x => x.DataHandler).Returns(dataHandler.Object);
+
+            var action = new BeginRecordAction();
+            var returnType = await action.ExecuteActionAsync(executionService.Object);
+
+            Assert.Equal(ExecutionReturnType.Continue, returnType);
+            executionService.Verify(x => x.DataHandler.AddRecord(), Times.Once());
+        }
+    }
+}
