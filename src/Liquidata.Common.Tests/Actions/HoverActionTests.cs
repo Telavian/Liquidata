e2f516c47a76c17b8075eb4b8ee9d2824d41ef39
Patrick Morris <telavian@gmail.com>
2024-07-05 14:11:48 -0700

    Added hover tests

---
+++ src/Liquidata.Common.Tests/Actions/HoverActionTests.cs
@@ -0,0 +1,46 @@
+using Liquidata.Common.Actions;
+using Liquidata.Common.Actions.Enums;
+using Liquidata.Common.Services.Interfaces;
+using Moq;
+
+namespace Liquidata.Common.Tests.Actions
+{
+    public class HoverActionTests
+    {
+        [Fact]
+        public async Task GivenCall_WhenValidation_ThenNoErrors()
+        {
+            await Task.Yield();
+            var action = new HoverAction();
+            var errors = action.BuildValidationErrors();
+
+            Assert.Empty(errors);
+        }
+
+        [Fact]
+        public async Task GivenCall_WhenDisabled_ThenNoAction()
+        {
+            var executionService = new Mock<IExecutionService>();
+
+            var action = new HoverAction { IsDisabled = true };
+            var returnType = await action.ExecuteActionAsync(executionService.Object);
+
+            Assert.Equal(ExecutionReturnType.Continue, returnType);
+        }
+
+        [Fact]
+        public async Task GivenCall_WhenExecuted_ThenRecordAdded()
+        {
+            var browser = new Mock<IBrowserService>();
+            
+            var executionService = new Mock<IExecutionService>();            
+            executionService.Setup(x => x.Browser).Returns(browser.Object);
+
+            var action = new HoverAction();
+            var returnType = await action.ExecuteActionAsync(executionService.Object);
+
+            Assert.Equal(ExecutionReturnType.Continue, returnType);
+            browser.Verify(x => x.HoverSelectionAsync(It.IsAny<string>()), Times.Once());
+        }
+    }
+}
