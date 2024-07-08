947355f3b7ed6f925ee80e99413bd4a65d8b3841
Patrick Morris <telavian@gmail.com>
2024-07-08 15:24:03 -0700

    Added enum tests

---
+++ src/Liquidata.Common.Tests/Extensions/EnumTests.cs
@@ -0,0 +1,25 @@
+﻿using Liquidata.Common.Extensions;
+
+namespace Liquidata.Common.Tests.Extensions;
+
+public class EnumTests
+{
+    [Fact]
+    public void GiveEnum_WhenLowercase_ThenTextLower()
+    {
+        var result = TestEnum.TestEnumValue.BuildFriendlyName(true);
+        Assert.Equal("test enum value", result);
+    }
+
+    [Fact]
+    public void GiveEnum_WhenUppercase_ThenTextUpper()
+    {
+        var result = TestEnum.TestEnumValue.BuildFriendlyName();
+        Assert.Equal("Test Enum Value", result);
+    }
+}
+
+enum TestEnum
+{
+    TestEnumValue,
+}
